ebdc221c1fa86a6ccab703b240b8bf29f81954c4
Giovanni <g.salmeri@mondodomani.org>
2022-01-02 18:12:20 +0100

    First release

---
+++ README.md
@@ -0,0 +1,60 @@
+Toml 0.8.18
+===========
+Minimal TOML parser.
+
+## How to use Toml from other extensions
+
+This extension provides the following TOML-related functions:
+
+`toml_parse($input)`  
+`toml_parse_file($fileName)`  
+`toml_parse_url($url)`  
+
+They return the value encoded in appropriate PHP type or `null` on failure. Errors can be accessed in `YellowToml::$error`.
+
+The most used features of TOML are supported: bare keys, dotted bare keys, basic strings, numbers, booleans, dates and times (returned as strings), arrays, tables, arrays of tables, and comments.
+
+Not supported are: quoted keys, literal and multiline strings, multiline arrays, inline tables.
+
+## Example
+
+```
+$toml = '
+# This is a TOML document
+
+title = "TOML Example"
+
+[owner]
+name = "Tom Preston-Werner"
+dob = 1979-05-27T07:32:00-08:00
+
+[database]
+enabled = true
+ports = [ 8000, 8001, 8002 ]
+data = [ ["delta", "phi"], [3.14] ]
+temp_targets.cpu = 79.5
+temp_targets.case = 72.0
+
+[servers]
+
+[servers.alpha]
+ip = "10.0.0.1"
+role = "frontend"
+
+[servers.beta]
+ip = "10.0.0.2"
+role = "backend"
+';
+
+$parsed = toml_parse($toml);
+```
+
+This is the same [example used in TOML site](https://toml.io/en/), adapted. Note the writing in two lines of `temp_targets`, since inline tables are not supported.
+
+## Installation
+
+[Download extension](https://github.com/GiovanniSalmeri/yellow-toml/archive/master.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+## Developer
+
+Giovanni Salmeri. [Get help](https://github.com/GiovanniSalmeri/yellow-toml/issues).
---
+++ toml.php
@@ -0,0 +1,109 @@
+<?php
+// Toml extension, https://github.com/GiovanniSalmeri/yellow-toml
+
+class YellowToml {
+    const VERSION = "0.8.18";
+
+    private static $lineNumber;
+    public static $error;
+
+    public static function parse($toml) {
+        $var = null;
+        $prefix = [];
+        $lines = preg_split('/\R/', $toml);
+        foreach ($lines as self::$lineNumber=>$line) {
+            if (trim($line)!=="" && $line[0]!=="#") {
+                if (preg_match('/^\s*([a-z\d_.\-]+?)\s*=\s*(.*?)\s*$/i', $line, $matches)) {
+                    $value = json_decode($matches[2], true);
+                    if ($value===null || self::isMap($value)) {
+                        if (strtotime($matches[2]) && preg_match('/^\d\d\d\d-\d\d-\d\d([T ]\d\d:\d\d:\d\d(\.\d{1,6})?(Z|[-+]\d\d:\d\d)?)?$/', $matches[2]) || preg_match('/^(\d\d):(\d\d):(\d\d)(\.\d{1,6})?$/', $matches[2], $parts) && $parts[1]<=23 && $parts[2]<=59 && $parts[3]<=59) {
+                            $value = $matches[2];
+                        } else {
+                            self::$error = "Invalid value at line ".(self::$lineNumber+1);
+                            return null;
+                        }
+                    }
+                    if (self::setDeep($var, array_merge($prefix, explode('.', $matches[1])), $value)===false) return null;
+                } elseif (preg_match('/^\s*\[([a-z\d_.\-]+?)\]\s*$/i', $line, $matches)) {
+                    $prefix = explode('.', $matches[1]);
+                    if (self::setDeep($var, $prefix, [])===false) return null;
+                } elseif (preg_match('/^\s*\[\[([a-z\d_.\-]+?)\]\]\s*$/i', $line, $matches)) {
+                    $prefix = explode('.', $matches[1]);
+                    $index = self::getIndex($var, $prefix);
+                    if ($index!==false) { 
+                        $prefix[] = (string)$index;
+                    } else {
+                        return null;
+                    }
+                    if (self::setDeep($var, $prefix, [])===false) return null;
+                } else {
+                    self::$error = "Invalid line ".(self::$lineNumber+1);
+                    return null;
+                }
+            }
+        }
+        return $var;
+    }
+
+    private static function isMap($value) {
+        if (is_array($value)) {
+            if ($value!==array_values($value)) {
+                self::$error = "Invalid value at line ".(self::$lineNumber+1);
+                return true;
+            } else {
+                foreach ($value as $item) {
+                    if (self::isMap($item)) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static function setDeep(&$var, $keysArray, $value) {
+        if (array_search("", $keysArray)!==false) {
+            self::$error = "Empty key at line ".(self::$lineNumber+1);
+            return false;
+        }
+        if (count($keysArray)==1) {
+            if (isset($var[$keysArray[0]])) {
+                self::$error = "Invalid re-assignement at line ".(self::$lineNumber+1);
+                return false;
+            } else {
+                $var[$keysArray[0]] = $value;
+            }
+        } else {
+            if (self::setDeep($var[$keysArray[0]], array_slice($keysArray, 1), $value)===false) return false;
+        }
+        return true;
+    }
+
+    private static function getIndex($var, $keysArray) {
+        if (array_search("", $keysArray)!==false) {
+            self::$error = "Empty key at line ".(self::$lineNumber+1);
+            return false;
+        }
+        if (count($keysArray)==1) {
+            if (!isset($var[$keysArray[0]])) {
+                return 0;
+            } elseif (is_array($var[$keysArray[0]]) && $var[$keysArray[0]]===array_values($var[$keysArray[0]])) {
+                return count($var[$keysArray[0]]);
+            } else {
+                self::$error = "Invalid re-assignement at line ".(self::$lineNumber+1);
+                return false;
+            }
+        } else {
+            return self::getIndex(@$var[$keysArray[0]], array_slice($keysArray, 1));
+        }
+    }
+}
+
+function toml_parse($input) {
+    return YellowToml::parse($input);
+}
+function toml_parse_file($filename) {
+    $content = file_get_contents($filename);
+    return $content===false ? false : YellowToml::parse($content);
+}
+function toml_parse_url($url) {
+    return toml_parse_file($url);
+}
